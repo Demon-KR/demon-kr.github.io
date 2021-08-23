ec05a2e62fb936fb395a87feab0aca4259ee3ce1
g0pher98 <mail@jseung.me>
2021-08-23 23:55:48 +0900

    디자인 다 빼고 심플하게 변경

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ function set_member() {
     fetch('/data/member.json').then(r=>r.json()).then(function(resp) {
         resp.forEach(function(member) {
             var $card = $(`
-            <div class="card">
-                <div class="photo"><img src="${member.photo}"></div>
-                <p>${member.name}</p>
-                <p>${member.nickname}</p>
-                <p>${member.introduce}</p>
-            </div>
+                <div class="card ${member.role}">
+                    <div class="photo"><img src="${member.photo}"></div>
+                    <p>${member.name}</p>
+                    <p>${member.nickname}</p>
+                    <p>${member.introduce}</p>
+                </div>
             `);
             // TODO : links 추가
             $('#member').append($card);
@@ -17,12 +17,20 @@ function set_member() {
 
 function set_active() {
     fetch('/data/active.json').then(r=>r.json()).then(function(resp) {
-        console.log(resp)
-        // TODO : active 추가
+        resp.forEach(function(active) {
+            var $card = $(`
+                <div class="card">
+                    <div><p>${active.date}</p></div>
+                    <div><p>${active.content}</p></div>
+                </div>
+            `);
+            // TODO : links 추가
+            $('#active').append($card);
+        });
     });
 }
 
 $(function() {
-    set_member()
-    set_active()
+    // set_member()
+    // set_active()
 })
