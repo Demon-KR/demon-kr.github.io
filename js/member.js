24c0f9cd13bf3f6b8940a43a1d451022f971e101
g0pher98 <mail@jseung.me>
2021-08-27 23:43:50 +0900

    각 멤버 card 형태로 분리

---
+++ js/member.js
@@ -0,0 +1,95 @@
+const MEMBER = [
+    {
+        "name": "정상수",
+        "nickname": "c0nstant",
+        "image": "/images/logo.jpg",
+        "discription": "음.. 뭐쓰지",
+        "links": {
+            "blog": "https://1993-constant.tistory.com/"
+        }
+    },
+    {
+        "name": "정동현",
+        "nickname": "jhyeon",
+        "image": "/images/logo.jpg",
+        "discription": "undefined",
+        "links": {
+            "github": "https://github.com/jhye0n",
+            "instagram": "https://instagram.com/jhye3n"
+        }
+    },
+    {
+        "name": "이재승",
+        "nickname": "g0pher",
+        "image": "/images/logo.jpg",
+        "discription": "킹갓해커가될거야",
+        "links": {
+            "github": "https://github.com/g0pher98",
+            "instagram": "https://instagram.com/g0pher_"
+        }
+    },
+    {
+        "name": "김주원",
+        "nickname": "arrester",
+        "image": "/images/logo.jpg",
+        "discription": "아따 함 봐준다",
+        "links": {
+            "blog": "https://blog.naver.com/lstarrlodyl",
+            "github": "https://github.com/arrester",
+            "facebook": "https://www.facebook.com/lstarrlodyl"
+        }
+    },
+    {
+        "name": "손현지",
+        "nickname": "jir4vvit",
+        "image": "/images/logo.jpg",
+        "discription": "-",
+        "links": {
+            "blog": "https://jiravvit.tistory.com/"
+        }
+    },
+    {
+        "name": "황수민",
+        "nickname": "Calibar",
+        "image": "/images/logo.jpg",
+        "discription": "-",
+        "links": {
+            "blog": "https://katolik-xixon.tistory.com/"
+        }
+    },
+    {
+        "name": "김준태",
+        "nickname": "racrua",
+        "image": "/images/logo.jpg",
+        "discription": "Hello, world!",
+        "links": {
+            "blog": "https://sunrinjuntae.tistory.com/",
+            "facebook": "https://www.facebook.com/sunrinjuntae"
+        }
+    }
+]
+
+
+$(function() {
+    MEMBER.forEach(function(member) {
+        console.log(member)
+        var $code = $(`
+            <div class="card">
+                <div class="profile_image">
+                    <img src="${member.image}" width="100%">
+                </div>
+                <div class="info">
+                    <p class="name">${member.name}</p>
+                    <p class="nickname">${member.nickname}</p>
+                </div>
+                <div class="discription">
+                    <p>${member.discription}</p>
+                </div>
+                <div class="contact">
+                </div>
+            </div>
+        `);
+        $('#members').append($code)
+    });
+    
+})
